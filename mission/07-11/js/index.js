5a6f195947963ba184b0cecb41bf80b69a5579f0
RedCrow-TABLET\Administrator <1064578256@qq.com>
2018-07-12 20:34:41 +0800

    updated

---
+++ mission/07-11/js/index.js
@@ -0,0 +1,28 @@
+$(function(){
+	$(".container").fullpage({
+//		设置内容顶部对齐
+		verticalCentered:false,
+		
+		afterLoad:function(anchor,index) {
+			if(index === 2) {
+				$(".question").find(".mark-1").addClass("show-mark-1");
+				$(".question").find(".mark-2").addClass("show-mark-2");
+				$(".question").find(".mark-5").addClass("show-mark-5");
+				$(".question").find(".mark-4").addClass("show-mark-4");
+				$(".question").find(".mark-3").addClass("show-mark-3");
+			}
+		}
+	});
+	
+//	开始测试
+	$(".start").click(function(){
+		$.fn.fullpage.moveSectionDown();
+	})
+	
+//	选择选项
+	$(".selection").click(function(){
+		$(this).find(".correct").css("display","block");
+		$(this).siblings(".selection").find(".correct").css("display","none");
+	})
+	
+})
