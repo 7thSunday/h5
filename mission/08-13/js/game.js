06a56fbaba047b867491d39b28ec80bdc003b99d
RedCrow-WS\Administrator <1064578256@qq.com>
2018-08-13 22:50:50 +0800

    updated

--- mission/08-13/js/game.js
+++ mission/08-13/js/game.js
@@ -1,67 +1,90 @@
-var deviceWidth = 0;
-var deviceHeight = 0;
-var gameframe = null;
-var gamePanel = null;
-// 初始化游戏面板
-function initPanel(frame,panel) {
-    deviceWidth = window.innerWidth;
-    deviceHeight = window.innerHeight;
-    gameframe = document.querySelector(frame);
-    gamePanel = document.getElementById(panel);
-    gameframe.style.height = deviceHeight + "px";
-    for(var j=0;j<10;j++) {
-        var newLine = document.createElement("tr");
-        newLine.style.height =deviceWidth/3 + "px";
-        var rand = Math.floor(Math.random()*4);
-        for(var i=0;i<4;i++) {
-            var newBlock = document.createElement("td");
-            if(i==rand&&j>1) {
-                newBlock.classList.add("target");
+function Game() {
+    this.deviceWidth = window.innerWidth;
+    this.deviceHeight = window.innerHeight;
+    this.gameframe = document.querySelector(frame);
+    this.gamePanel = document.getElementById(panel);
+    this.btnStart = document.querySelector(".btn-start");
+    // 初始化游戏面板
+    function initPanel() {
+        this.gamePanel.innerHTML = "";
+        this.gameframe.style.height = deviceHeight + "px";
+        this.btnStart.style.display = "block";
+        for(var j=0;j<10;j++) {
+            var newLine = document.createElement("tr");
+            newLine.style.height =deviceWidth/3 + "px";
+            var rand = Math.floor(Math.random()*4);
+            for(var i=0;i<4;i++) {
+                var newBlock = document.createElement("td");
+                if(i==rand&&j>1) {
+                    newBlock.classList.add("target");
+                }
+                newBlock.style.height =deviceWidth/3 + "px";
+                newBlock.style.width =deviceWidth/4 + "px";
+                newLine.appendChild(newBlock);
             }
-            newBlock.style.height =deviceWidth/3 + "px";
-            newBlock.style.width =deviceWidth/4 + "px";
-            newLine.appendChild(newBlock);
+            gamePanel.appendChild(newLine);
         }
-        gamePanel.appendChild(newLine);
+    }
+
+    // 开始游戏
+    function start() {
+        var speed = 1;
+        var step = deviceWidth/3;
+        var distance = step;
+        
+        var levelup = setInterval(function() {
+            speed = speed*0.8;
+            console.log("speed up!",speed);
+        },10000);
+        gamePanel.style = "transition: all " + speed*0.8*1000 + "ms linear 0s";
+        gamePanel.style.transform = "translateY(" + distance + "px)";
+
+        gamePanel.addEventListener("transitionend",function() {
+            var newLine = document.createElement("tr");
+            this.style = "transition: all " + speed*0.8*1000 + "ms linear 0s";
+            newLine.style.height = deviceWidth/3 + "px";
+            var rand = Math.floor(Math.random()*4);
+            for(var i=0;i<4;i++) {
+                var newBlock = document.createElement("td");
+                if(i==rand) {
+                    newBlock.classList.add("target");
+                }
+                newBlock.style.height = deviceWidth/3 + "px";
+                newBlock.style.width = deviceWidth/4 + "px";
+                newLine.appendChild(newBlock);
+            }
+            panel.appendChild(newLine);
+            distance += step;
+            gamePanel.style.transform = "translateY(" + distance + "px)";
+        });
+
+        // 方块的点击事件
+        gamePanel.addEventListener("click",function(e){
+            var preLine = e.target.parentNode.previousSibling.children;
+            var allow = true;
+            for(var i=0;i<4;i++) {
+                if(preLine[i].classList.contains("target")) {
+                    allow = false;
+                }
+            }
+            if(allow) {
+                if(e.target.classList.contains("target")) {
+                    e.target.classList.remove("target");
+                } else {
+                    console.log("gg");
+                    gameOver(levelup);
+                }
+            }
+        });
+    }
+
+    // 结束游戏
+    function gameOver(levelup) {
+        alert("Game Over!");
+        clearInterval(levelup);
+        gamePanel.style = "";
+        gameframe = null;
+        gamePanel = null;
+        initPanel(".frame","panel");
     }
 }
-
-// 开始游戏
-function start() {
-    var speed = 1;
-    var step = deviceWidth/3;
-    var distance = step;
-    
-    var levelup = setInterval(function() {
-        speed = speed*0.8;
-        console.log("speed up!",speed);
-    },10000);
-    gamePanel.style = "transition: all " + speed*0.8*1000 + "ms linear 0s";
-    gamePanel.style.transform = "translateY(" + distance + "px)";
-
-    gamePanel.addEventListener("transitionend",function() {
-        var newLine = document.createElement("tr");
-        this.style = "transition: all " + speed*0.8*1000 + "ms linear 0s";
-        newLine.style.height = deviceWidth/3 + "px";
-        var rand = Math.floor(Math.random()*4);
-        for(var i=0;i<4;i++) {
-            var newBlock = document.createElement("td");
-            if(i==rand) {
-                newBlock.classList.add("target");
-            }
-            newBlock.style.height = deviceWidth/3 + "px";
-            newBlock.style.width = deviceWidth/4 + "px";
-            newLine.appendChild(newBlock);
-        }
-        panel.appendChild(newLine);
-        distance += step;
-        gamePanel.style.transform = "translateY(" + distance + "px)";
-    });
-
-    // 黑块点击事件
-    gamePanel.addEventListener("click",function(e){
-        // console.log(e.target.parentNode);
-        // if(e.target.parentNode)
-    });
-}
-
--- mission/08-13/js/index.js
+++ mission/08-13/js/index.js
@@ -1,10 +1,10 @@
 function run() {
-    initPanel(".frame","panel");
-    var btnStart = document.querySelector(".btn-start");
-    btnStart.addEventListener("click",function() {
-        console.log("start!");
-        this.style.display = "none";
-        start();
-    });
+    // initPanel(".frame","panel");
+    // var btnStart = document.querySelector(".btn-start");
+    // btnStart.addEventListener("click",function() {
+    //     console.log("start!");
+    //     this.style.display = "none";
+    //     start();
+    // });
 }
 run();
