c4ad40773858bdf9a8ce2b0ca3ca55371e2d579e
RedCrow-WS\Administrator <1064578256@qq.com>
2018-10-04 11:22:07 +0800

    update mapledemo

--- mapledemo/js/maple.js
+++ mapledemo/js/maple.js
@@ -1,4 +1,67 @@
 class Maple {
-    constructor() {}
-    drop() {}
+    constructor(canvas,texture) {
+        this.canvas = canvas;
+        this.ctx = this.canvas.getContext('2d');
+        this.texture = texture;
+        this.w = texture.width / 8;
+        this.h = texture.height / 8;
+        this.init();
+    }
+    // initialize
+    init() {
+        // position
+        this.x = Math.floor(Math.random()*this.canvas.width);
+        this.y = Math.floor(Math.random()*this.canvas.height -200);
+        // scale size
+        this.size = Math.random()*0.4 + 0.8;
+        // rotate
+        this.rotateDer = Math.random()>0.5 ? true : false;
+        this.rotateDeg = 0;
+        this.rotateSpeed = Math.random() / 50;
+        // drop
+        this.dropDer = Math.random()>0.5 ? true : false;
+        this.dropSpeed = Math.random()*0.2+2;
+        this.xSpeed = Math.random() / 1.6;
+        // existence time
+        this.extTime = Math.floor(Math.random()*3+3)*1000;
+        this.crtTime = new Date().getTime();
+        // alpha
+        this.alpha = 0;
+        this.maxAlpha = 0.8;
+        // initialize completed and start dropping
+        this.drop();
+    }
+    // function drop
+    drop() {
+        // rotate
+        this.rotateDer ? this.rotateDeg += this.rotateSpeed : this.rotateDeg -= this.rotateSpeed;
+        // x pivot
+        this.dropDer ? this.x += this.xSpeed : this.x -= this.xSpeed;
+        // y pivot
+        this.y += this.dropSpeed;
+    }
+    // draw on canvas
+    draw() {
+        let time = new Date().getTime();
+        // drop or init
+        if(time-this.crtTime < this.extTime) {
+            this.drop();
+        }else {
+            this.init();
+        }
+        // alpha animation
+        if(time-this.crtTime<1000&&this.alpha<this.maxAlpha) {
+            this.alpha += 0.02;
+        }else if((time+1000)>(this.crtTime+this.extTime)&&this.alpha>0) {
+            this.alpha -= 0.02;
+        }
+        // draw image while alpha over 0
+        if(this.alpha>0) {
+            this.ctx.globalAlpha = this.alpha;
+            this.ctx.translate(this.x,this.y);
+            this.ctx.rotate(Math.PI * this.rotateDeg);
+            this.ctx.scale(this.size,this.size);
+            this.ctx.drawImage(this.texture,-this.w/2,-this.h/2,this.w,this.h);
+        }
+    }
 }
--- mapledemo/js/scene.js
+++ mapledemo/js/scene.js
@@ -1,10 +1,14 @@
 class Scene {
-    constructor(canvas) {
+    constructor(canvas,src) {
         this.canvas = document.getElementById(canvas);
         this.devWidth = window.innerWidth;
         this.devHeight = window.innerHeight;
         this.ctx = null;
-        this.src = ["../img/sample01.png","../img/sample02.png"];
+        this.src = src;
+        this.img = [];
+        this.fps = 60;// refresh rate
+        this.count = 100;// amount of maple leaf
+        this.texture = [];// render texture
     }
     // initialize
     init() {
@@ -17,26 +21,45 @@ class Scene {
         // init canvas
         this.canvas.width = this.devWidth;
         this.canvas.height = this.devHeight;
-        this.canvas.style = `position: absolute;top: 0;left: 0;`
+        this.canvas.style = `position: fixed;top: 0;left: 0;`;
+        this.ctx.save();
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
         // run after loaded
-        let loading = new createjs.LoadQueue();
-        loading.loadManifest(this.src);
-        loading.on("complete",() => {
-            this.run();
-        });
-        loading.on("error", () => {
-            window.alert("load failed.");
-        });
-    }
-    // run scene
-    run() {
-        let type1 = new Image(),
-            type2 = new Image();
-        type1.src = this.src[0];
-        type2.src = this.src[1];
-        // this.ctx.clearRect(0,0,this.devWidth,this.devHeight);
-        type1.onload = () => {
-            this.ctx.drawImage(type1,0,0);
+        let length = this.src.length;
+        let $i = 0;
+        for(let url of this.src) {
+            let img = new Image();
+            img.src = url;
+            img.onload = () => {
+                this.img.push(img);
+                $i ++;
+                if($i==length) {
+                    this.start();
+                }
+            }
         }
+    }
+    // start scene
+    start() {
+        for(let i=0;i<this.count;i++) {
+            let type = Math.floor(Math.random()*this.img.length);
+            let maple = new Maple(this.canvas,this.img[type]);
+            this.texture.push(maple);
+        }
+        this.run();
+    }
+    // run loop
+    run() {
+        this.ctx.restore();
+        this.ctx.save();
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        for(let i=0;i<this.count;i++) {
+            this.ctx.restore();
+            this.ctx.save();
+            this.texture[i].draw();
+        }
+        setTimeout( ()=> {
+            this.run();
+        },1000/this.fps);
     }
 }
